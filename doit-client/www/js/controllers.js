989aaca28ab63a406ec848cc70188101c2ec89d7
Albrey Brown <albrey.brown@gmail.com>
2014-07-30 19:23:29 -0700

    added first shell of clientside

---
+++ doit-client/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('doit.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+})
+
+.controller('LoginCtrl', function($scope, $state){
+  $scope.login = function(){
+    $state.go('tab.dash');
+  };
+  
+});
