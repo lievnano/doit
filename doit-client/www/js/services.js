dd13e4d2b34ce18396f2ec81975c560a30de0319
Albrey Brown <albrey.brown@gmail.com>
2014-07-31 14:41:42 -0700

    added friends in friends

--- doit-client/www/js/services.js
+++ doit-client/www/js/services.js
@@ -2,21 +2,20 @@ angular.module('doit.services', [])
 
 .factory('Friends', function(){
   
-  var friends = {
-    Thomas: {
+  var friends = [
+      {
       name: 'Thomas',
       img:'http://upload.wikimedia.org/wikipedia/commons/9/92/George_Clooney-4_The_Men_Who_Stare_at_Goats_TIFF09_(cropped).jpg'},
-    Kad: {
+      {
       name: 'Kad',
       img:'http://weadiamedia.com/wp-content/uploads/2014/04/george-clooney-1.jpg'},
-    Suzanne: {
+      {
       name: 'Suzanne',
       img: 'http://images.boomsbeat.com/data/images/full/51818/clooney_george-jpg.jpg'},
-    Leon: {
+      {
       name: "Leon",
       img:'http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/2/7/1391786293758/George-Clooney---Save-the-012.jpg'},
-  };
-
+  ];
 
   return friends;
 });
