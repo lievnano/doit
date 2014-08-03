b6836ef7cf02d978d4e806d873e2a8b3833c761f
liev nano <lievnano@gmail.com>
2014-08-02 17:53:42 -0700

    setup skeleton of server

---
+++ doit-server/server.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+ *
+ * Entry file into the server
+ * @app -
+ *    our express app. Exported for testing and flexibility.
+ *
+*/
+
+// This file sets up the server
+var app   = require('./main/app.js'),
+    port  = app.get('port'),
+    log   = 'Listening on ' + app.get('base url') + ':' + port;
+
+app.listen(port);
+console.log(log);
