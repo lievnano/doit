56dd96e3c2cab192ac3aeebd21c3d983a58b9202
liev nano <lievnano@gmail.com>
2014-08-19 17:09:32 -0700

    added responses for curator and user controller

--- doit-server/curator/curator_controller.js
+++ doit-server/curator/curator_controller.js
@@ -5,12 +5,47 @@ module.exports = exports = {
     next();
   },
   addPlace : function(req, res){
-    
+    //check variables are good...
+    dbOps.addPlace(req.body.locationID, req.body.placeName, req.body.address, req.body.description, req.body.imgLink, 
+                  function(err,rows){
+                    if (err){
+                      res.send(500,err);
+                    }
+                    else{
+                      res.send(200,rows);
+                    }
+                  });
   },
   addActivity : function(req,res){
-
+    //the following line is long... contains all params from addActicity in dbOps, callback function on second line 
+    dbOps.addActivity(req.body.activityName, req.body.description, req.body.uniquePlace, req.body.placeCategoryID, req.body.placeID, req.body.imgLink, req.body.status, req.body.participantsNeeded, req.body.occursOnce, req.body.startDateTime, req.body.endDateTime, req.body.openingTime, req.body.closingTime, req.body.minDuration, req.body.maxDuration, req.body.typeID,
+                      function(err,rows){
+                        if (err){
+                          res.send(500,err);
+                        }
+                        else{
+                          res.send(200,rows);
+                        }
+                      });
   },
   getPlaces : function(req,res){
-    
-  }
+    dbOps.getPlaces(function(err, rows){
+      if (err){
+        res.send(500,err);
+      }
+      else{
+        res.send(200,rows);
+      }
+    });
+  },
+  getTypes  : function(req,res){
+    dbOps.getTypes(function(err,rows){
+      if (err){
+        res.send(500,err);
+      }
+      else{
+        res.send(200,rows);
+      }
+    });
+  },
 };
--- doit-server/user/user_controller.js
+++ doit-server/user/user_controller.js
@@ -2,17 +2,82 @@ var dbOps = require('../dbOperations/adminDB');
 
 module.exports = exports = {
   isUser : function(req,res,next){
+    //TODO ** check that userID given in req is legit
     next();
   },
-  getPreviousActivities : function(req, res){
-    
-  },
-  addActivity : function(req,res){
+  getCurrentActivities : function(req, res){
+    //do check on request to make sure its good
+
+    //send request to adminDB function and then send the resposne back to client
+    dbOps.getUserCurrent(req.body.userID, function(err,rows){
+      if (err){
+        res.send(500,err);
+      }
+      else{
+        res.send(200,rows);
+      }
+    });
 
   },
+  getPreviousActivities : function(req, res){
+    //do check on request to make sure its good
+
+    //send request to adminDB function 
+    //check that their dateTime hasn't expired
+    //and then send the resposne back to client
+    dbOps.getUserPrevious(req.body.userID, function(err,rows){
+      if (err){
+        res.send(500,err);
+      }
+      else{
+        res.send(200,rows);
+      }
+    });
+  },
+  addActivity : function(req,res){
+    //TODO ** do check on request to make sure its good
+
+    //send request to adminDB function and then confirm to client that activity has been added...
+    dbOps.setUserCurrent(req.body.userID, req.body.activityID, req.body.startDateTime,req.body.duration, req.body.placeID), function(err,rows){
+      if (err){
+        res.send(500,err);
+      }
+      else{
+        res.send(200,rows);
+      }
+    }
+  },
   getNewActivities : function(req,res){
+    //TODO ** do check on request to make sure its good
+
+
+    //send request to adminDB function and then send response back to client
+    dbOps.getUserActivities(req.body.userID, req.body.locationID, req.body.whenStart, req.body.duration, req.body.typeID, req.body.dateTimeToDo, req.body.timeToDo, function(err, rows){
+      if (err){
+        res.send(500, err);
+      }
+      else {
+        res.send(200, rows);
+      }
+
+    });
+
+
      // getUserActivities : function(userID, locationID, whenStart, duration, typeID, dateTimeToDo, timeToDo, callback){
      console.log(req.body);
      // doOps.getUserActivities()
+  },
+  updateActivityToCompleted : function(req,res){
+    //TODO ** do check on request to make sure its good
+
+    //send request to adminDB function and then send response back to client
+    dbOps.updateActivityToCompleted(req.body.userID, req.body.userActivityID, req.body.endTime, function(err, rows){
+      if (err){
+        res.send(500,err)
+      }
+      else{
+        res.send(200,rows);
+      }
+    });
   }
 };
