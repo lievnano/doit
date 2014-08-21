08e784c47f939279c3949253ceedc07bd899dded
liev nano <lievnano@gmail.com>
2014-08-20 20:08:45 -0700

    took out standardCallback function calls  didn't work because it didn't have access to exports req/res scope...

--- doit-server/user/user_controller.js
+++ doit-server/user/user_controller.js
@@ -1,15 +1,6 @@
 var dbOps = require('../dbOperations/adminDB');
 
 
-function standardCallback(err,rows){
-  if(err){
-    console.log(err);
-    res.status(500).send(err);
-  }
-  else{
-    res.status(200).send(rows);
-  }
-};
 
 module.exports = exports = {
   isUser : function(req,res,next){
@@ -25,7 +16,15 @@ module.exports = exports = {
     //do check on request to make sure its good
 
     //send request to adminDB function and then send the resposne back to client
-    dbOps.getUserCurrent(req.body.userID, standardCallback);
+    dbOps.getUserCurrent(req.body.userID, function(err,rows){
+      if(err){
+        console.log(err);
+        res.status(500).send(err);
+      }
+      else{
+        res.status(200).send(rows);
+      }
+    });
 
   },
   getPreviousActivities : function(req, res){
@@ -34,19 +33,43 @@ module.exports = exports = {
     //send request to adminDB function 
     //check that their dateTime hasn't expired
     //and then send the resposne back to client
-    dbOps.getUserPrevious(req.body.userID, standardCallback);
+    dbOps.getUserPrevious(req.body.userID, function(err,rows){
+      if(err){
+        console.log(err);
+        res.status(500).send(err);
+      }
+      else{
+        res.status(200).send(rows);
+      }
+    });
   },
   addRating : function(req,res){
     //do check on request to make sure its good
 
-    dbOps.addRatingToActivity(req.body.userID, req.body.userActivityID, req.body.rating, standardCallback);
+    dbOps.addRatingToActivity(req.body.userID, req.body.userActivityID, req.body.rating, function(err,rows){
+      if(err){
+        console.log(err);
+        res.status(500).send(err);
+      }
+      else{
+        res.status(200).send(rows);
+      }
+    });
   },
   addActivity : function(req,res){
     //TODO ** do check on request to make sure its good
 
     //send request to adminDB function and then confirm to client that activity has been added...
     dbOps.setUserCurrent(req.body.userID, req.body.activityID, req.body.startDateTime,req.body.duration,
-     req.body.placeID, standardCallback); 
+     req.body.placeID, function(err,rows){
+      if(err){
+        console.log(err);
+        res.status(500).send(err);
+      }
+      else{
+        res.status(200).send(rows);
+      }
+    });
   },
   getNewActivities : function(req,res){
     //TODO ** do check on request to make sure its good
@@ -55,13 +78,29 @@ module.exports = exports = {
     //send request to adminDB function and then send response back to client
     dbOps.getUserActivities(req.body.userID, req.body.locationID, req.body.whenStart, 
       req.body.duration, req.body.typeID, req.body.dateTimeToDo, req.body.timeToDo,
-      standardCallback);
+      function(err,rows){
+      if(err){
+        console.log(err);
+        res.status(500).send(err);
+      }
+      else{
+        res.status(200).send(rows);
+      }
+    });
   },
   updateActivityToCompleted : function(req,res){
     //TODO ** do check on request to make sure its good
 
     //send request to adminDB function and then send response back to client
     dbOps.updateActivityToCompleted(req.body.userID, req.body.userActivityID, req.body.endTime, 
-      standardCallback);
+      function(err,rows){
+        if(err){
+          console.log(err);
+          res.status(500).send(err);
+        }
+        else{
+          res.status(200).send(rows);
+        }
+      });
   },
 };
